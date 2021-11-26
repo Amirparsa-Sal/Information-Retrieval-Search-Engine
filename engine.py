61e9342d0aee24977c1243789aff5622c08953b6
Amirparsa-Sal <Amirparsa.s751@gmail.com>
2021-11-26 11:40:08 +0330

    Remove links from data and add data excel file

--- engine.py
+++ engine.py
@@ -1,18 +1,28 @@
 from __future__ import unicode_literals
-from codecs import register
-from hazm import Normalizer,Stemmer,word_tokenize
+from hazm import Normalizer, word_tokenize
 import string
-
-from hazm.Lemmatizer import Lemmatizer
+import openpyxl
+from parsivar import FindStems
+import re
 
 normalizer = Normalizer()
-stemmer = Stemmer()
-lemmatizer = Lemmatizer()
-index = dict()
+stemmer = FindStems()
+index = dict() #token -> [freq, [doc_id, freq, pos1, pos2, ...], [doc_id, freq, pos1, pos2, ...], ...]
+EXCEL_FILE_NAME = 'data.xlsx'
+LINK_REGEX = r"((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z]){2,6}([ا-یa-zA-Z0-9\.\&\/\?\:@\-_=# ])*"
+
 
 def perform_linguistic_preprocessing(text):
+    text = re.sub(LINK_REGEX, '', text)
     text = text.translate(str.maketrans('', '', string.punctuation))
     text = normalizer.normalize(text)
     token_list = word_tokenize(text)
-    token_list = list(map(lambda word: lemmatizer.lemmatize(word), token_list))
+    token_list = list(map(lambda word: stemmer.convert_to_stem(word), token_list))
     return token_list
+
+wb = openpyxl.load_workbook(EXCEL_FILE_NAME)
+sheet = wb.active
+
+
+# content = sheet.cell(row=2, column=1).value
+# print(perform_linguistic_preprocessing(content))
