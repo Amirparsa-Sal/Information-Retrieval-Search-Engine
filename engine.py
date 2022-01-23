1230a45b9073767102a3e81d1a860b90083989c1
Amirparsa-Sal <Amirparsa.s751@gmail.com>
2022-01-23 20:16:14 +0330

    handle different columns in excel file and optimize memory

--- engine.py
+++ engine.py
@@ -15,12 +15,23 @@ normalizer = Normalizer()
 stemmer = FindStems()
 index = dict() #token -> [freq, {doc_id1: [freq, pos1, pos2, ...], doc_id2: [freq, pos1, pos2, ...], ...}, doc_freq]
 length_arr = []
-EXCEL_FILE_NAME = 'data.xlsx'
+EXCEL_FILE_NAME = 'Merged.xlsx'
 LINK_REGEX = r"((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z]){2,6}([ا-یa-zA-Z0-9\.\&\/\?\:@\-_=# ])*"
 punctuations = string.punctuation
 punctuations += ''.join(['،','؛','»','«','؟'])
 CHAMPIONS_LIST_SIZE=  20
 champions_list = dict()
+
+def fill_column_numbers(sheet):
+    columns_dic = dict()
+
+    for i in range (1,5):
+        columns_dic[sheet.cell(row=1, column=i).value] = i
+
+    if 'title' not in columns_dic:
+        columns_dic['title'] = columns_dic['url']
+
+    return columns_dic
 
 def perform_linguistic_preprocessing(text, delete_stop_words=True, stemming=True):
     '''
@@ -52,7 +63,7 @@ def perform_linguistic_preprocessing(text, delete_stop_words=True, stemming=True
 
     return token_positions
 
-def create_index(sheet, index, delete_stop_words=True):
+def create_index(sheet, index, content_column, delete_stop_words=True):
     '''
     A function to create an index from the excel file.\n
     Args:\n
@@ -61,7 +72,7 @@ def create_index(sheet, index, delete_stop_words=True):
     '''
     # Looping over the excel file
     for i in range(2, sheet.max_row + 1):
-        content = sheet.cell(row=i, column=1).value
+        content = sheet.cell(row=i, column=content_column).value
         # Performing linguistic preprocessing on the content
         token_list = perform_linguistic_preprocessing(content, delete_stop_words=delete_stop_words)
         # Looping over the tokens
@@ -271,9 +282,6 @@ def ranked_retreival_search(doc_number, query, index_elimination_threshold=0.0, 
             # Add tf.id for each query item and its documents
             for doc_id in all_docs_containing_terms:
                 if doc_id in index[item[0]][1]:
-                    if doc_id == '119':
-                        print(item[0], doc_id, 1 + math.log10(index[item[0]][1][doc_id][0]), index[item[0]][1][doc_id][0], math.log10(doc_number / index[item[0]][2]))
-                        print(index[item[0]][1][doc_id])
                     w_dt = (1 + math.log10(index[item[0]][1][doc_id][0])) * math.log10(doc_number / index[item[0]][2])
                     tf_idf = w_tq * w_dt
                     scores[int(doc_id) - 1] += tf_idf
@@ -283,14 +291,12 @@ def ranked_retreival_search(doc_number, query, index_elimination_threshold=0.0, 
             scores[i] /= length_arr[i]
     # sort the scores
     scores_sorted = sorted(enumerate(scores), key=lambda x: x[1], reverse=True)
-    for i in range (10):
-        print(scores_sorted[i][0], scores_sorted[i][1], length_arr[scores_sorted[i][0]])
     # return the top 10 results
     return [str(x[0] + 1) for x in scores_sorted[:10]]
 
-def search(query, sheet, ranked=True, index_eliminiation_threshold=0.0, doc_coverage_threshold=0.6, use_champions_list=False):
+def search(query, max_row, ranked=True, index_eliminiation_threshold=0.0, doc_coverage_threshold=0.6, use_champions_list=False):
     if ranked:
-        return ranked_retreival_search(sheet.max_row - 1, query, index_elimination_threshold=index_eliminiation_threshold, doc_coverage_threshold=doc_coverage_threshold, use_champions_list=use_champions_list)
+        return ranked_retreival_search(max_row, query, index_elimination_threshold=index_eliminiation_threshold, doc_coverage_threshold=doc_coverage_threshold, use_champions_list=use_champions_list)
     return multiple_word_query(query)
 
 def plot_zipf_law(index):
@@ -306,12 +312,12 @@ def plot_zipf_law(index):
     plt.plot(ranks, count_multiply_rank)
     plt.show()
 
-def count_tokens_and_text_length(sheet, n, stemming=True):
+def count_tokens_and_text_length(sheet, n, content_column, stemming=True):
     '''A function to count the number of tokens and the length of the text.'''
     tokens = dict()
     length = 0
     for i in range(2, 2 + n):
-        content = sheet.cell(row=i, column=1).value
+        content = sheet.cell(row=i, column=content_column).value
         length += len(word_tokenize(content))
         token_list = perform_linguistic_preprocessing(content, stemming=stemming)
         for token in token_list:
@@ -319,13 +325,14 @@ def count_tokens_and_text_length(sheet, n, stemming=True):
     return len(tokens), length
 
 if __name__ == '__main__':
-    print(perform_linguistic_preprocessing('آیا مسی بهترین بازیکن جهان است؟'))
     wb = openpyxl.load_workbook(EXCEL_FILE_NAME)
     sheet = wb.active
 
+    columns_dic = fill_column_numbers(sheet)
+
     if not os.path.exists('index.json'):
         print('Creating index...')
-        create_index(sheet, index, delete_stop_words=True)
+        create_index(sheet, index, columns_dic['content'], delete_stop_words=True)
     else:
         print('Reading index...')
         index = read_dic_from_file('index.json')
@@ -340,16 +347,19 @@ if __name__ == '__main__':
     print('Creating length array...')
     length_arr = create_length_arr(sheet, index)
 
+    max_row = sheet.max_row - 1
+    wb.close()
+
     while True:
         query = input('Enter your query: ')
         query = list(map(lambda token: token[0], perform_linguistic_preprocessing(query)))
         if len(query) != 0:
-            news = search(query, sheet, ranked=True, index_eliminiation_threshold=0.0, doc_coverage_threshold=0.6, use_champions_list=True)
+            news = search(query, max_row, ranked=True, index_eliminiation_threshold=0.0, doc_coverage_threshold=0.6, use_champions_list=True)
             if news is None:
                 print('No news found')
             else:
                 print('News found:')
-                news = list(map(lambda doc_id: (sheet.cell(row=int(doc_id)+1, column=2).value, sheet.cell(row=int(doc_id)+1, column=3).value, doc_id), news))
+                news = list(map(lambda doc_id: (sheet.cell(row=int(doc_id)+1, column=columns_dic['url']).value, sheet.cell(row=int(doc_id)+1, column=columns_dic['title']).value, doc_id), news))
                 for i,new in enumerate(news):
                     print(new[2] + ': ' + new[1])
                     print(new[0])
--- word_embedding.py
+++ word_embedding.py
@@ -1,4 +1,4 @@
-from engine import perform_linguistic_preprocessing, read_dic_from_file
+from engine import perform_linguistic_preprocessing, read_dic_from_file, fill_column_numbers
 import openpyxl
 import pickle
 import math
@@ -8,18 +8,17 @@ from gensim.models import Word2Vec
 import os
 import multiprocessing
 import time 
-import matplotlib.pyplot as plt
 
 EXCEL_FILE_NAME = 'data.xlsx'
 wb = openpyxl.load_workbook(EXCEL_FILE_NAME)
 sheet = wb.active
 MODEL_FILE = 'insa_news.model'
 
-def create_tokens_list(sheet, delete_stop_words=True):
+def create_tokens_list(sheet, content_column, delete_stop_words=True):
     doc_token_list = []
     # Looping over the excel file
     for i in range(2, sheet.max_row + 1):
-        content = sheet.cell(row=i, column=1).value
+        content = sheet.cell(row=i, column=content_column).value
         # Performing linguistic preprocessing on the content
         token_list = perform_linguistic_preprocessing(content, delete_stop_words=delete_stop_words)
         only_tokens_list = [item[0] for item in token_list]
@@ -27,11 +26,11 @@ def create_tokens_list(sheet, delete_stop_words=True):
     pickle.dump(doc_token_list, open("token_list.obj", "wb"))
     return doc_token_list
 
-def create_token_list_with_count(sheet, delete_stop_words=True):
+def create_token_list_with_count(sheet, content_column, delete_stop_words=True):
     doc_token_list_with_counts = []
     # Looping over the excel file
     for i in range(2, sheet.max_row + 1):
-        content = sheet.cell(row=i, column=1).value
+        content = sheet.cell(row=i, column=content_column).value
         # Performing linguistic preprocessing on the content
         token_list = perform_linguistic_preprocessing(content, delete_stop_words=delete_stop_words)
         token_list = [item[0] for item in token_list]
@@ -41,13 +40,21 @@ def create_token_list_with_count(sheet, delete_stop_words=True):
     return doc_token_list_with_counts
 
 
-def create_tf_idf_list(doc_number):
-    index = read_dic_from_file('index.json')
-    index = {key: index[key][2] for key in index}
-    doc_token_list_with_counts = create_token_list_with_count(sheet)
+def create_token_count_dic(docs_token_list_with_count):
+    token_count_dic = dict()
+    for doc in docs_token_list_with_count:
+        for token in doc:
+            if token in token_count_dic:
+                token_count_dic[token] += doc[token]
+            else:
+                token_count_dic[token] = doc[token]
+    pickle.dump(token_count_dic, open("token_count.obj", "wb"))
+    return token_count_dic
+
+def create_tf_idf_list(doc_number, doc_token_list_with_counts, token_count):
     for dic in doc_token_list_with_counts:
         for key in dic:
-            dic[key] = (1 + math.log10(dic[key])) * math.log10(doc_number / index[key])
+            dic[key] = (1 + math.log10(dic[key])) * math.log10(doc_number / token_count[key])
     pickle.dump(doc_token_list_with_counts, open("tf_idf_dic.obj", "wb"))
     return doc_token_list_with_counts
 
@@ -70,14 +77,12 @@ def similarity(matrix, doc_1, doc_2):
     score = np.dot(matrix[:, doc_1 - 1].T, matrix[:, doc_2 - 1]) / (norm(matrix[:, doc_1 - 1]) * norm(matrix[:, doc_2 - 1]))
     return (score + 1) / 2
 
-def search(model, matrix, query, doc_number):
-    index = read_dic_from_file('index.json')
-    index = {key: index[key][2] for key in index}
+def create_query_vector(model, query, token_count_dic, doc_number):
     # Calculate tf-idf for the query
     query_tf_idf_dic = dict(zip(query, [query.count(item) for item in query]))
     for key in query_tf_idf_dic:
-        if key in index:
-            query_tf_idf_dic[key] = (1 + math.log10(query_tf_idf_dic[key])) * math.log10(doc_number / index[key])
+        if key in token_count_dic:
+            query_tf_idf_dic[key] = (1 + math.log10(query_tf_idf_dic[key])) * math.log10(doc_number / token_count_dic[key])
 
     query_vector = np.zeros((300))
     weight_sum = 0
@@ -87,6 +92,10 @@ def search(model, matrix, query, doc_number):
             weight_sum += query_tf_idf_dic[key]
     if weight_sum != 0:
         query_vector /= weight_sum
+    return query_vector
+
+def search(model, matrix, query, token_count_dic, doc_number):
+    query_vector = create_query_vector(model, query, token_count_dic, doc_number)
     scores = [0 for i in range(matrix.shape[1])]
     for i in range(matrix.shape[1]):
         scores[i] += np.dot(query_vector.T, matrix[:, i]) / (norm(matrix[:, i]) * norm(query_vector))
@@ -95,12 +104,36 @@ def search(model, matrix, query, doc_number):
     return [(str(score[0] + 1), score[1]) for score in scores[:min(10, len(scores))]]
 
 if __name__ == '__main__':
-    doc_token_list = []
+    columns_dic = fill_column_numbers(sheet)
+    
+    docs_token_list_with_counts = dict()
+    if not os.path.exists("token_list_count.obj"):
+        print("Creating token list with count")
+        docs_token_list_with_counts = create_token_list_with_count(sheet, columns_dic['content'], delete_stop_words=True)
+    else:
+        docs_token_list_with_counts = pickle.load(open("token_list_count.obj", "rb"))
+
+    token_count_dic = dict()
+    if not os.path.exists('token_count.obj'):
+        print("Creating token count")
+        token_count_dic = create_token_count_dic(docs_token_list_with_counts)
+    else:
+        token_count_dic = pickle.load(open("token_count.obj", "rb"))
+    
+    tf_idf_list = []
+    if not os.path.exists("tf_idf_dic.obj"):
+        print("Creating tf-idf list")
+        tf_idf_list = create_tf_idf_list(len(docs_token_list_with_counts), docs_token_list_with_counts, token_count_dic)
+    else:
+        tf_idf_list = pickle.load(open("tf_idf_dic.obj", "rb"))
+
+
+    docs_token_list = []
     if not os.path.exists("token_list.obj"):
         print("Creating token list")
-        doc_token_list = create_tokens_list(sheet)
+        docs_token_list = create_tokens_list(sheet, columns_dic['content'])
     else:
-        doc_token_list = pickle.load(open("token_list.obj", "rb"))
+        docs_token_list = pickle.load(open("token_list.obj", "rb"))
 
     model = None
     if not os.path.exists(MODEL_FILE):
@@ -109,10 +142,10 @@ if __name__ == '__main__':
                             vector_size = 300,
                             alpha = 0.03,
                             workers = multiprocessing.cpu_count() - 1)
-        model.build_vocab(doc_token_list)
+        model.build_vocab(docs_token_list)
         print('Start Training model...')
         start = time.time()
-        model.train(doc_token_list, total_examples = model.corpus_count, epochs = 30)
+        model.train(docs_token_list, total_examples = model.corpus_count, epochs = 30)
         end = time.time()
         print(f'Completed in {(end - start)} s')
         model.save(MODEL_FILE)
@@ -120,29 +153,24 @@ if __name__ == '__main__':
     else:
         print("Loading model")
         model = Word2Vec.load(MODEL_FILE)
-
     
-    tf_idf_list = []
-    if not os.path.exists("tf_idf_dic.obj"):
-        print("Creating tf-idf list")
-        tf_idf_list = create_tf_idf_list(len(doc_token_list))
-    else:
-        tf_idf_list = pickle.load(open("tf_idf_dic.obj", "rb"))
-    
-    del doc_token_list
+    del docs_token_list
 
     matrix = create_docs_matrix(model, tf_idf_list)
     
+    del tf_idf_list
+    del docs_token_list_with_counts
+
     while True:
         query = input('Enter your query: ')
         query = list(map(lambda token: token[0], perform_linguistic_preprocessing(query)))
         if len(query) != 0:
-            news = search(model, matrix, query, sheet.max_row - 1)
+            news = search(model, matrix, query, token_count_dic, sheet.max_row - 1)
             if news is None:
                 print('No news found')
             else:
                 print('News found:')
-                news = list(map(lambda new: (sheet.cell(row=int(new[0])+1, column=2).value, sheet.cell(row=int(new[0])+1, column=3).value, new[0], new[1]), news))
+                news = list(map(lambda new: (sheet.cell(row=int(new[0])+1, column=columns_dic['url']).value, sheet.cell(row=int(new[0])+1, column=columns_dic['title']).value, new[0], new[1]), news))
                 for i,new in enumerate(news):
                     print(new[2] + ': ' + new[1])
                     print(f"Similarity: {new[3]}")
